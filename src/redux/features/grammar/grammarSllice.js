f8a20a77bb4c710ff3bc60dd7d1000d0c8a09afd
pheng bunath <bunathpheng4@gmail.com>
2024-08-21 10:18:27 +0700

    change env with new domain name

--- src/redux/features/grammar/grammarSllice.js
+++ src/redux/features/grammar/grammarSllice.js
@@ -15,8 +15,7 @@ const apiUrl = import.meta.env.VITE_BASE_URL.replace(/^http:/, "https:");
 export const fetchGrammars = createAsyncThunk(
   "grammars/fetchGrammars", // Make sure this matches the slice name
   async () => {
-    const response = await fetch('https://english-api.cstad.shop/grammars');
-
+    const response = await fetch(`${apiUrl}/grammars`);
     const data = await response.json();
     return data.payload;
     
@@ -26,7 +25,7 @@ export const fetchGrammars = createAsyncThunk(
 export const fetchGrammarsByLevel = createAsyncThunk(
   "grammars/fetchGrammarsByLevel", // Make sure this matches the slice name
   async (level) => {
-    const response = await fetch(`https://english-api.cstad.shop/grammars/${level}`);
+    const response = await fetch(`${apiUrl}/grammars/${level}`);
     const data = await response.json();
     return data.payload;
   }
